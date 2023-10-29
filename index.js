0b23293dea72e45d21a48a328e9a680c85fd4014
Subhajit <subha.pramanik1212@gmail.com>
2023-10-30 02:14:01 +0530

    Code commited for change in routes index.js

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'));
+
+
+
+
 // Listening on port
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error to listen: ${err}`);
   }
-  
   console.log(`Express server listening on port: ${port}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router  = express.Router();
+
+console.log("router started");
+
+
+
+
+
+module.exports = router;
