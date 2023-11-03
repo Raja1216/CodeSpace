6ba5d97578622455ec399984dbf01bc5b8625898
Subhajit <shubhajit.pramanik@edudigm.in>
2023-11-03 05:36:13 -0700

    Code commited for Menual Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,20 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("user_profile", {
-    title: "Profile",
-  });
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id)
+      .then((result) => {
+        return res.render("user_profile", {
+          title: "Profile",
+          user_details: result,
+        });
+      })
+      .catch((err) => {
+        return res.redirect("/users/sign-in");
+      });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
 };
 
 //render Sign In Page
@@ -29,7 +40,6 @@ module.exports.create = function (req, res) {
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-
         User.create(req.body)
           .then((result) => {
             return res.redirect("/users/sign-in");
@@ -38,7 +48,6 @@ module.exports.create = function (req, res) {
             console.log("Error in creating user in sign Up", err);
             return;
           });
-        
       } else {
         return res.redirect("back");
       }
@@ -50,4 +59,37 @@ module.exports.create = function (req, res) {
 };
 
 // get the sign In data
-module.exports.createSession = function (req, res) {};
+module.exports.createSession = function (req, res) {
+  //find the user
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      //handle user found
+      if (user) {
+        //handle password which don't match
+        if (user.password != req.body.password) {
+          return res.redirect("back");
+        }
+        //handle session creation
+        res.cookie("user_id", user._id);
+        return res.redirect("/users/profile");
+      } else {
+        //handle user not found
+        return res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log("Error in finding user in sign in", err);
+      return;
+    });
+};
+
+//destroy session
+
+module.exports.destroySession = function (req, res) {
+  if (req.cookies.user_id) {
+    res.cookie("user_id", "");
+    return res.redirect("/users/sign-in");
+  } else {
+    return res.redirect("back");
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -7,8 +7,10 @@ const userController = require("../controllers/users_controller");
 router.get("/profile", userController.profile);
 router.get("/sign-in", userController.signIn);
 router.get("/sign-up", userController.signUp);
+router.get("/sign-out", userController.destroySession);
 
 
-router.post('/create', userController.create);
+router.post("/create", userController.create);
+router.post("/create-session", userController.createSession);
 
 module.exports = router;
