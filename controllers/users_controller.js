9e444376adb04fad392ed2f9e3a36b5137cdf90b
Subhajit <shubhajit.pramanik@edudigm.in>
2023-11-03 04:57:47 -0700

    Code commited for setup Mongo

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,53 @@
+const User = require("../models/user");
+
 module.exports.profile = function (req, res) {
-    return res.render('user_profile', {
-        title: "Profile"
+  return res.render("user_profile", {
+    title: "Profile",
+  });
+};
+
+//render Sign In Page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "Sign In",
+  });
+};
+
+//render Sign Up Page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "Sign Up",
+  });
+};
+
+// get the sign up data
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+
+        User.create(req.body)
+          .then((result) => {
+            return res.redirect("/users/sign-in");
+          })
+          .catch((err) => {
+            console.log("Error in creating user in sign Up", err);
+            return;
+          });
+        
+      } else {
+        return res.redirect("back");
+      }
+    })
+    .catch((err) => {
+      console.log("Error in finding user in sign Up", err);
+      return;
     });
-}
+};
+
+// get the sign In data
+module.exports.createSession = function (req, res) {};
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
+app.use(express.urlencoded())
+
+app.use(cookieParser());
 
 //Static file call
 app.use(express.static('./assets'));
@@ -16,6 +21,7 @@ app.set("layout extractScripts", true);
 
 //use express router
 app.use('/', require('./routes'));
+
 //use View Engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
